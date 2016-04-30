3239bb744ef6c5ae8d283cee36bd0803a7d143d2
seungkwon <yevgnenll@gmail.com>
2016-04-30 23:07:04 +0900

    모듈화
    api만을 보여주는 api 모듈,
    등록, 수정, 삭제 등을 보여주기 위한
    controller를 위해 모듈화를 하였습니다.

--- archive/views/__init__.py
+++ archive/views/__init__.py
@@ -1,4 +1,1 @@
-from .home import index
-from .images import images_list
-from .error_handler import error404, error500
-from .artists import artist_list
+from .api import *
---
+++ archive/views/api/__init__.py
@@ -0,0 +1,4 @@
+from .home import index
+from .images import images_list
+from .error_handler import error404, error500
+from .artists import artist_list
